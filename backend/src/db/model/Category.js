14e7f324dde17509a0ab80b9a7a819a3d6479591
kdilot <kdilot@gmail.com>
2019-02-23 18:59:47 -0800

    db model

--- backend/src/db/model/Category.js
+++ backend/src/db/model/Category.js
@@ -19,11 +19,15 @@ CategorySchema.statics.drop = function () {
 }
 
 CategorySchema.statics.showAll = function (cb) {
-    return this.find({}, cb)
+    return this.find({ display: 1 }, cb)
 }
 
-CategorySchema.statics.updateCategory = function (_id, data) {
-    return this.findOneAndUpdate({ _id }, { ...data, lastUpdate: new Date() }, { upsert: false, new: true }).exec()
+CategorySchema.statics.removeCategory = function (_id) {
+    return this.findOneAndUpdate({ _id }, { display: 0, lastUpdate: new Date() }, { upsert: false, new: true }).exec()
+}
+
+CategorySchema.statics.editCategory = function (_id, title) {
+    return this.findOneAndUpdate({ _id }, { title, lastUpdate: new Date() }, { upsert: false, new: true }).exec()
 }
 
 module.exports = mongoose.model('category', CategorySchema)
