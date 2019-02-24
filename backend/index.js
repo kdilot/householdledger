029174c989ce73e8ac652e811960dca2aaa34b53
kdilot <kdilot@gmail.com>
2019-02-23 19:00:23 -0800

    index

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const port = process.env.PORT || 3001;
+const db = require('db');
+const api = require('api');
+const server = express()
+    .use(express.static(path.join(__dirname, 'client/build')))
+    .use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+    .use(bodyParser.json())
+    .use('/api', api)
+    .use((req, res) => res.sendFile(path.join(__dirname + '/client/build/index.html')))
+    .listen(port, () => console.log(`Listening on ${port}`));
+db.connect();
