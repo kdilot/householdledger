a626ee8b04cbe6da846963673e51f6b7b4bba248
kdilot <kdilot@gmail.com>
2019-03-04 15:38:38 -0800

    db model

--- backend/src/db/model/Data.js
+++ backend/src/db/model/Data.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const autoIncrement = require('mongoose-auto-increment')
+const moment = require('moment')
 const Schema = mongoose.Schema
 
 connection = mongoose.createConnection(process.env.MLAB_MGDB_URL, { useCreateIndex: true, useNewUrlParser: true })
@@ -8,8 +9,8 @@ autoIncrement.initialize(connection)
 const DataSchema = new Schema({
     id: { type: Number, ref: 'Data' },
     title: String,
-    description: String,
     category: Number,
+    type: Boolean,
     price: Number,
     display: Number,
     lastUpdate: Date
@@ -21,8 +22,13 @@ DataSchema.statics.drop = function () {
     return this.collection.drop()
 }
 
-DataSchema.statics.showAll = function (cb) {
-    return this.find({}, cb)
+DataSchema.statics.showAll = function (today) {
+    const date = moment(today).set('date', 1).set('date', 1).set('hour', 0).set('minute', 0).set('second', 0)
+    return this.find({ "display": 1, "lastUpdate": { "$gte": date, "$lt": moment(date).add(1, 'month') } }).sort({ "lastUpdate": 1 })
+}
+
+DataSchema.statics.removeData = function (_id) {
+    return this.findOneAndUpdate({ _id }, { display: 0, lastUpdate: new Date() }, { upsert: false, new: true }).exec()
 }
 
 DataSchema.statics.updateData = function (_id, data) {
@@ -30,7 +36,3 @@ DataSchema.statics.updateData = function (_id, data) {
 }
 
 module.exports = mongoose.model('data', DataSchema)
-
-// CurrencyRateSchema.statics.updateTicker = function (name, data) {
-//   return this.findOneAndUpdate({ name }, { ...data, lastUpdate: new Date() }, { upsert: false, new: true }).exec()
-// }
