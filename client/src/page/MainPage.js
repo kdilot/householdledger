984eaaa5f4240d388e343687b042d1c81c167f34
kdilot <kdilot@gmail.com>
2019-03-10 12:26:36 -0700

    view page

--- client/src/page/MainPage.js
+++ client/src/page/MainPage.js
@@ -1,38 +1,40 @@
 import React, { Component } from 'react';
 import { Button, Row, Col, Icon } from 'antd';
+import Context from 'context';
 import '../main.scss';
 
 class MainPage extends Component {
   render() {
-    const {
-      today,
-      changeMonth,
-      changeFlag
-    } = this.props
     return (
-      <div className="Head">
-        <header className="Head-header">
-          <h1>
-            <Row>
-              <Col xs={0} sm={0} md={7} lg={7} xl={7} className="alignRight">
-                <Icon type="caret-left" onClick={() => { changeMonth() }} />
-              </Col>
-              <Col xs={24} sm={24} md={10} lg={10} xl={10}>
-                {today.format('YYYY.MM')}
-              </Col>
-              <Col xs={12} sm={12} md={0} lg={0} xl={0} className="alignRight">
-                <Icon type="caret-left" onClick={() => { changeMonth() }} />
-              </Col>
-              <Col xs={12} sm={12} md={7} lg={7} xl={7} className="alignLeft">
-                <Icon type="caret-right" onClick={() => { changeMonth(1) }} />
-              </Col>
-            </Row>
-          </h1>
-          <h2>
-            <Button type="primary" size='large' onClick={() => { changeFlag() }}>Enter</Button>
-          </h2>
-        </header>
-      </div>
+      <Context.Consumer>
+        {c => {
+          return (
+            <div className="Head">
+              <header className="Head-header">
+                <h1>
+                  <Row>
+                    <Col xs={0} sm={0} md={7} lg={7} xl={7} className="alignRight">
+                      <Icon type="caret-left" onClick={() => { c.changeMonth() }} />
+                    </Col>
+                    <Col xs={24} sm={24} md={10} lg={10} xl={10}>
+                      {c.today.format('YYYY.MM')}
+                    </Col>
+                    <Col xs={12} sm={12} md={0} lg={0} xl={0} className="alignRight">
+                      <Icon type="caret-left" onClick={() => { c.changeMonth() }} />
+                    </Col>
+                    <Col xs={12} sm={12} md={7} lg={7} xl={7} className="alignLeft">
+                      <Icon type="caret-right" onClick={() => { c.changeMonth(1) }} />
+                    </Col>
+                  </Row>
+                </h1>
+                <h2>
+                  <Button type="primary" size='large' onClick={() => { c.changeFlag() }}>Enter</Button>
+                </h2>
+              </header>
+            </div>
+          )
+        }}
+      </Context.Consumer>
     );
   }
 }
