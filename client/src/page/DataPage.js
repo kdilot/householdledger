ebb0c668896f70a726142eaea65cdd85afba48ef
kdilot <kdilot@gmail.com>
2019-03-16 01:04:47 -0700

    view

--- client/src/page/DataPage.js
+++ client/src/page/DataPage.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
-import { Card, Col, Icon, Button, Form, Empty, message, Statistic } from 'antd';
+import { Card, Row, Col, Icon, Button, Form, Empty, message, Statistic } from 'antd';
 import moment from 'moment';
 import { CurrencyFormat, DrawerFormat } from 'common';
 import { ApiPost } from 'common/Api';
 const monthFormat = 'MM/DD/YYYY';
+const ButtonGroup = Button.Group;
 
 class DataPage extends Component {
   getData = () => {
     ApiPost('data-list', { today: this.props.today }).then(res => {
+      const { asc, sort } = this.state
       const income = res.data.filter(f => f.type === true)
       const expense = res.data.filter(f => f.type === false)
+      const data = res.data.sort((a, b) => {
+        if (sort === 'date') {
+          return asc ? moment(a.lastUpdate) - moment(b.lastUpdate) : moment(b.lastUpdate) - moment(a.lastUpdate)
+        } else if (sort === 'title') {
+          return asc ? a.title.localeCompare(b.title) : b.title.localeCompare(a.title)
+        } else if (sort === 'price') {
+          return asc ? a.price - b.price : b.price - a.price
+        }
+        return true
+      })
       this.setState({
-        data: res.data,
+        data: data,
         income: income.length > 1 ? income.reduce((a, b) => a + b.price, 0) : income.length === 1 ? income[0].price : 0,
         expense: expense.length > 1 ? expense.reduce((a, b) => a + b.price, 0) : expense.length === 1 ? expense[0].price : 0
       })
@@ -42,16 +54,33 @@ class DataPage extends Component {
       selectedData: selected._id ? selected : null
     })
   }
+  changeSort = (isASC = true, keyword) => {
+    if (isASC) {
+      const { asc } = this.state
+      this.setState({ asc: !asc })
+    } else {
+      const { asc, sort } = this.state
+      if (sort === keyword)
+        this.setState({ asc: !asc })
+      else
+        this.setState({ sort: keyword })
+    }
+    this.getData()
+  }
+
   state = {
     data: [],
     income: [],
     expense: [],
     visible: false,
     selectedData: null,
+    sort: 'date',
+    asc: true,
     getData: this.getData,
     removeData: this.removeData,
     showDrawer: this.showDrawer,
     handleSubmit: this.handleSubmit,
+    changeSort: this.changeSort,
   }
   componentDidMount() {
     this.getData()
@@ -64,28 +93,39 @@ class DataPage extends Component {
   render() {
     const {
       data,
-      removeData,
       income,
       expense,
+      sort,
+      asc,
+      removeData,
       showDrawer,
+      changeSort,
     } = this.state
     const {
       categoryData,
     } = this.props
     return (
       <div className="drawer">
-        <div className="alignLeft">
-          <Button type="primary" onClick={() => { showDrawer() }}>Add</Button>
-        </div>
-        <div className="totalForm alignLeft bold">
-          <Statistic title="Income" value={income} precision={2} valueStyle={{ color: 'green' }} prefix="$" />
-          <Statistic title="Expense" value={expense} precision={2} valueStyle={{ color: 'red' }} prefix="$" />
-          <Statistic title="Change" value={expense > 0 || income > 0 ? ((income - expense) / expense) * 100 : 0} precision={2} valueStyle={{ color: 'gold' }} suffix="%" />
-        </div>
         <DrawerFormat
           state={this.state}
           props={this.props}
         />
+        <Row className="alignLeft">
+          <Button type="primary" onClick={() => { showDrawer() }}>Add</Button>
+        </Row>
+        <Row className="totalForm alignLeft bold">
+          <Statistic title="Income" value={income} precision={2} valueStyle={{ color: 'green' }} prefix="$" />
+          <Statistic title="Expense" value={expense} precision={2} valueStyle={{ color: 'red' }} prefix="$" />
+          <Statistic title="Change" value={expense > 0 || income > 0 ? ((income - expense) / expense) * 100 : 0} precision={2} valueStyle={{ color: 'gold' }} suffix="%" />
+        </Row>
+        <Row className="alignLeft dataSort">
+          <ButtonGroup>
+            <Button type={sort === 'date' ? 'default' : 'primary'} onClick={() => { changeSort(false, 'date') }}>Date</Button>
+            <Button type={sort === 'title' ? 'default' : 'primary'} onClick={() => { changeSort(false, 'title') }}>description</Button>
+            <Button type={sort === 'price' ? 'default' : 'primary'} onClick={() => { changeSort(false, 'price') }}>Price</Button>
+            <Button type="default" onClick={() => { changeSort() }}><Icon type={`arrow-${asc ? 'up' : 'down'}`} /></Button>
+          </ButtonGroup>
+        </Row>
         <Card className="dataList">
           {data.length > 0 ?
             data.map((m, i) => {
