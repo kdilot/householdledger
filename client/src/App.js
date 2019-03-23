0761f47c3373f7b9876f0594a5ed48714839a93d
kdilot <kdilot@gmail.com>
2019-03-23 14:50:32 -0700

    default

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,69 @@
 import React, { Component } from 'react';
 import { MainPage, PageList } from 'page';
 import moment from 'moment';
+import Context from 'context';
+import { ApiGet } from 'common/Api';
 import './main.scss';
 
 class App extends Component {
+  getCategoryList = () => {
+    ApiGet('category-list').then(res => {
+      this.setState({ categoryData: res.data })
+    })
+  }
   changeFlag = () => {
     const { flag } = this.state
     this.setState({ flag: !flag })
   }
+  changeTab = (key) => {
+    if (key === '3') {
+      const { tabFlag } = this.state
+      this.setState({ tabFlag: !tabFlag })
+    }
+  }
   changeDate = (date) => {
-    this.setState({ today: date })
+    if (date)
+      this.setState({ today: date })
   }
   changeMonth = (plus) => {
     const { today } = this.state
     plus ?
       this.setState({
-        today: today.add(1, 'month')
+        today: today.add(1, 'month').set('date', 1)
       })
       :
       this.setState({
-        today: today.add(-1, 'month')
+        today: today.add(-1, 'month').set('date', 1)
       })
   }
   state = {
     flag: true,
+    tabFlag: true,
     today: moment(),
+    categoryData: [],
+    changeTab: this.changeTab,
     changeDate: this.changeDate,
     changeMonth: this.changeMonth,
-    changeFlag: this.changeFlag
+    changeFlag: this.changeFlag,
+    getCategoryList: this.getCategoryList,
+  }
+  componentDidMount() {
+    this.getCategoryList()
   }
   render() {
     const {
       flag,
     } = this.state
     return (
-      <div className="Head">
-        {flag ?
-          <MainPage {...this.state} />
-          :
-          <PageList {...this.state} />
-        }
-      </div>
+      <Context.Provider value={this.state}>
+        <div className="Head">
+          {flag ?
+            <MainPage />
+            :
+            <PageList />
+          }
+        </div>
+      </Context.Provider>
     );
   }
 }
--- client/src/common/index.js
+++ client/src/common/index.js
@@ -1,2 +1,3 @@
 export { default as CurrencyFormat } from './CurrencyFormat'
 export { default as DrawerFormat } from './DrawerFormat'
+export { default as ReportFormat } from './ReportFormat'
