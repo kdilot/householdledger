5f03ae00562b91efaff3a847d7f96f8036eec669
kdilot <kdilot@gmail.com>
2019-03-24 15:21:11 -0700

    view

--- client/src/page/CategoryPage.js
+++ client/src/page/CategoryPage.js
@@ -1,31 +1,26 @@
 import React, { Component } from 'react';
-import { Icon, Button, Empty, Col, message, Input, Alert } from 'antd';
+import { Icon, Button, Spin, Col, message, Input, Alert } from 'antd';
 import moment from 'moment';
 import { ApiGet, ApiPost } from 'common/Api';
 
 class CategoryPage extends Component {
-  getCategoryList = () => {
-    ApiGet('category-list').then(res => {
-      this.setState({ apiData: res.data })
-    })
-  }
   addCategory = () => {
     ApiGet('category-add').then(res => {
       res.status === 200 ? message.success('Category Add Success') : message.error('Category Add Fail')
-      this.getCategoryList()
+      this.props.getCategoryList()
     })
   }
   removeCategory = (_id) => {
     ApiPost('category-remove', { _id }).then(res => {
       res.status === 200 ? message.success('Category Remove Success') : message.error('Category Remove Fail')
-      this.getCategoryList()
+      this.props.getCategoryList()
     })
   }
   editCategory = (value, _id) => {
     if (value && _id) {
       ApiPost('category-edit', { value, _id }).then(res => {
         res.status === 200 ? message.success('Category Edit Success') : message.error('Category Edit Fail')
-        this.getCategoryList()
+        this.props.getCategoryList()
         this.setState({ editable: '' })
       })
     }
@@ -45,29 +40,22 @@ class CategoryPage extends Component {
   state = {
     editable: '',
     effect: '',
-    apiData: [],
-    getCategoryList: this.getCategoryList,
     addCategory: this.addCategory,
     removeCategory: this.removeCategory,
     editCategory: this.editCategory,
     selectEditable: this.selectEditable,
   }
-  componentDidMount() {
-    this.getCategoryList()
-  }
   componentDidUpdate(prevProps, prevState) {
-    if (prevState.apiData.length === this.state.apiData.length) {
-      prevState.apiData.map((m, i) =>
-        m.title !== this.state.apiData[i].title ? this.highlight(m._id) : ''
+    if (prevProps.categoryData.length === this.props.categoryData.length) {
+      prevProps.categoryData.map((m, i) =>
+        m.title !== this.props.categoryData[i].title ? this.highlight(m._id) : ''
       )
-    } else if (prevState.apiData.length > 0 && prevState.apiData.length < this.state.apiData.length) {
-      this.highlight(this.state.apiData[this.state.apiData.length - 1]._id)
+    } else if (prevProps.categoryData.length > 0 && prevProps.categoryData.length < this.props.categoryData.length) {
+      this.highlight(this.props.categoryData[this.props.categoryData.length - 1]._id)
     }
   }
-
   render() {
     const {
-      apiData,
       editable,
       addCategory,
       removeCategory,
@@ -75,25 +63,14 @@ class CategoryPage extends Component {
       selectEditable,
       effect
     } = this.state
+    const {
+      categoryData
+    } = this.props
     return (
       <div>
         <Col xs={0} sm={0} md={5} lg={5} xl={5} />
         <Col xs={24} sm={24} md={14} lg={14} xl={14}>
-          <div className="alignLeft">
-            <Button type="primary" onClick={() => { addCategory() }}>Add</Button>
-          </div>
-          {apiData.length === 0 ? <Empty /> :
-            apiData.map((list, index) => {
-              return (
-                <div className={effect === list._id ? 'divFormat categoryList highlight' : 'divFormat categoryList'} key={index}>
-                  <h1 className="cursor-hand" onClick={() => removeCategory(list._id)}><Icon type="close-square" style={{ fontSize: '1.5em', color: 'red' }} /></h1>
-                  <h1 className="cursor-hand" onClick={() => { selectEditable(index) }}>{editable === index ? <Input placeholder={list.title} ref={(r) => { this.inputRef = r }} autoFocus={true} allowClear onPressEnter={() => { editCategory(this.inputRef.state.value, list._id) }} /> : list.title}</h1>
-                  <h2>{moment(list.lastUpdate).format('(MM/DD/YYYY HH:mm:ss)')}</h2>
-                </div>
-              )
-            })
-          }
-          {apiData.length > 0 &&
+          {categoryData.length > 0 &&
             <Alert
               className="warning"
               showIcon
@@ -101,6 +78,20 @@ class CategoryPage extends Component {
               type="warning"
               closable
             />
+          }
+          <div className="alignLeft">
+            <Button type="primary" onClick={() => { addCategory() }}>Add</Button>
+          </div>
+          {categoryData.length === 0 ? <Spin /> :
+            categoryData.map((list, index) => {
+              return (
+                <div className={effect === list._id ? 'divFormat categoryList highlight' : 'divFormat categoryList'} key={index}>
+                  <h1 className="cursor-hand" onClick={() => removeCategory(list._id)}><Icon type="close-square" style={{ fontSize: '1.5em', color: 'red' }} /></h1>
+                  <h1 className="cursor-hand" onClick={() => { selectEditable(index) }}>{editable === index ? <Input placeholder={list.title} ref={(r) => { this.inputRef = r }} autoFocus={true} allowClear onPressEnter={() => { editCategory(this.inputRef.state.value, list._id) }} /> : list.title}</h1>
+                  <h2>{moment(list.lastUpdate).format('(MM/DD/YYYY)')}</h2>
+                </div>
+              )
+            })
           }
         </Col>
         <Col xs={0} sm={0} md={5} lg={5} xl={5} />
