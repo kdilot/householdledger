bf5c57b95d0c758da3be65be949362811fe0cd01
kdilot <kdilot@gmail.com>
2019-03-25 14:13:09 -0700

    view

--- client/src/page/PageList.js
+++ client/src/page/PageList.js
@@ -1,29 +1,30 @@
 import React, { Component } from 'react';
-import { DataPage, CategoryPage } from 'page';
+import { DataPage, CategoryPage, ReportPage } from 'page';
 import { Button, Tabs, DatePicker } from 'antd';
-import { WithWidget } from 'common';
 import moment from 'moment';
+import Context from 'context';
 import './page.scss';
 const TabPane = Tabs.TabPane
-const monthFormat = 'MM/DD/YYYY';
+const monthFormat = 'MM/DD/YYYY'
 
 class PageList extends Component {
   render() {
-    const {
-      today,
-      changeDate,
-      changeFlag
-    } = this.props
-    const Test = WithWidget(<CategoryPage />)
     return (
-      <header className="Head-tab">
-        <DatePicker defaultValue={moment(today, monthFormat)} format={monthFormat} onChange={(date) => { changeDate(date) }} />
-        <Tabs defaultActiveKey="1" tabBarExtraContent={<Button type="primary" onClick={() => { changeFlag() }}>Back</Button>} style={{ width: '100%' }}>
-          <TabPane tab="Data" key="1"><DataPage today={today} /></TabPane>
-          <TabPane tab="Category" key="2"><CategoryPage {...this.state} /></TabPane>
-          <TabPane tab="Report" key="3"><Test /></TabPane>
-        </Tabs>
-      </header>
+      <Context.Consumer>
+        {c => {
+          return (
+            <header className="Head-tab">
+              <h1 className="bold">{c.today.format('MMM YYYY')}</h1>
+              <DatePicker value={moment(c.today)} format={monthFormat} onChange={(date) => { c.changeDate(date) }} />
+              <Tabs defaultActiveKey="1" tabBarExtraContent={<Button type="primary" onClick={() => { c.changeFlag() }} >Back</Button>} onTabClick={(key) => { c.changeTab(key) }} style={{ width: '100%' }}>
+                <TabPane tab="Data" key="1"><DataPage {...c} /></TabPane>
+                <TabPane tab="Category" key="2"><CategoryPage {...c} /></TabPane>
+                <TabPane tab="Report" key="3"><ReportPage {...c} /></TabPane>
+              </Tabs>
+            </header>
+          )
+        }}
+      </Context.Consumer>
     );
   }
 }
