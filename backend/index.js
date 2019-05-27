3c6cee782d499b1961a23be1ca4d484b046c44a3
kdilot <kdilot@gmail.com>
2019-05-27 15:14:34 -0700

    index

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,12 @@ const server = express()
     .use(express.static(path.join(__dirname, 'client/build')))
     .use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
     .use(bodyParser.json())
+    .use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*")
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+        next()
+    })
     .use('/api', api)
     .use((req, res) => res.sendFile(path.join(__dirname + '/client/build/index.html')))
-    .listen(port, () => console.log(`Listening on ${port}`));
+    .listen(port, () => console.log(`Listening on ${port}`))
 db.connect();
