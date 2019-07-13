9275bf84ac2d24d9513aa16439e42504e52e9491
kdilot <kdilot@gmail.com>
2019-07-13 13:53:43 -0700

    component

--- client/src/page/PageList.js
+++ client/src/page/PageList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { DataPage, CategoryPage, ReportPage } from 'page';
-import { Button, Tabs, DatePicker } from 'antd';
+import { Tabs, DatePicker } from 'antd';
 import moment from 'moment';
 import Context from 'context';
 import './page.scss';
@@ -16,7 +16,7 @@ class PageList extends Component {
             <header className="Head-tab">
               <h1 className="bold">{c.today.format('MMM YYYY')}</h1>
               <DatePicker value={moment(c.today)} format={monthFormat} onChange={(date) => { c.changeDate(date) }} />
-              <Tabs defaultActiveKey="1" tabBarExtraContent={<Button type="primary" onClick={() => { c.changeFlag() }} >Back</Button>} onTabClick={(key) => { c.changeTab(key) }} style={{ width: '100%' }}>
+              <Tabs defaultActiveKey="1" onTabClick={(key) => { c.changeTab(key) }} style={{ width: '100%' }}>
                 <TabPane tab="Data" key="1"><DataPage {...c} /></TabPane>
                 <TabPane tab="Category" key="2"><CategoryPage {...c} /></TabPane>
                 <TabPane tab="Report" key="3"><ReportPage {...c} /></TabPane>
